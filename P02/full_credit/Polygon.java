680c697376534cda9cf1c8db940de4302dfbc52e
Kolade Favour <fok0786@mavs.uta.edu>
2022-01-27 01:16:46 -0600

    P02 full credit

--- P02/full_credit/Polygon.java
+++ P02/full_credit/Polygon.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Polygon {
+	/*public static int addSides(int x) {
+		int sum = 0;
+		sum = sum + x;
+		return sum;
+	}*/
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+
+		int s = 0;
+		int sides = 1;
+		int sum = 0;
+		double perimeter = 0.0;
+
+		while(sides != 0){
+			System.out.print("Side length (0 when done): ");
+			sides = in.nextInt(); in.nextLine();
+			s++;
+			sum+=sides;
+			perimeter = (double)sum;
+		}
+		System.out.println("Perimeter of a " + (s - 1) + "-sided polygon is " + perimeter);
 	}
 }
