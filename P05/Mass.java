ce327909fd283209ae9661f42e6e7d6a78737a2e
Kolade Favour <fok0786@mavs.uta.edu>
2022-02-28 11:43:03 -0600

    Scrum project 01

---
+++ P05/Mass.java
@@ -0,0 +1,15 @@
+import shelter.Dog;
+import shelter.Cat;
+
+import java.util.Scanner;
+
+public class Mass {
+	public static void main(String[] args) {
+		String name;
+
+		Scanner in = new Scanner(System.in);
+
+		name = in.nextLine();
+		System.out.println(name);
+	}
+}
