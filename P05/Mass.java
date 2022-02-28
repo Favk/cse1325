521b82f6bb950a5cc98a532615c27bc24846ce6c
Kolade Favour <fok0786@mavs.uta.edu>
2022-02-28 16:53:52 -0600

    Scrum project 01

--- P05/Mass.java
+++ P05/Mass.java
@@ -1,15 +1,30 @@
 import shelter.Dog;
 import shelter.Cat;
+import shelter.Shelter;
 
 import java.util.Scanner;
 
 public class Mass {
 	public static void main(String[] args) {
 		String name;
+		String breed;
+		String gender;
+		int age;
 
 		Scanner in = new Scanner(System.in);
 
 		name = in.nextLine();
 		System.out.println(name);
+
+		breed = in.nextLine();
+		DogBreed breed = DogBreed.valueOf(breed);
+
+		gender = in.nextLine();
+		Gender gender = Gender.valueOf(gender);
+
+		age = in.nextInt(); in.nextLine();
+
+		Dog aDog = new Dog(breed, name, gender, age);
+
 	}
 }
