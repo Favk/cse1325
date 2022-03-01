5bbc83bcb87696d27a090dc532fabf5f34ef3918
Kolade Favour <fok0786@mavs.uta.edu>
2022-02-28 22:26:15 -0600

    Scrum project 01

--- P05/Mass.java
+++ P05/Mass.java
@@ -1,31 +1,61 @@
 import shelter.Dog;
 import shelter.Cat;
 import shelter.Shelter;
+import shelter.DogBreed;
+import shelter.CatBreed;
+import shelter.Gender;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Mass {
 	public static void main(String[] args) {
 		String name;
 		String breed;
 		String gender;
+		String ShelterName = "Arlington Loves Animal Companions";
 		int age;
+
+		Shelter shelter = new Shelter(ShelterName);
 
 		Scanner in = new Scanner(System.in);
 
+		System.out.print("Enter name of dog: ");
 		name = in.nextLine();
-		System.out.println(name);
 
+		System.out.print("Enter a breed of dog from one of the following: Dalmatian, Greyhound, Chihuahua, Boxer, Husky, Samoyed, Dobermann, Rottweiler, Corgi, Schnauzer: ");
 		breed = in.nextLine();
-		DogBreed breed = DogBreed.valueOf(breed);
+		DogBreed breedOfAnimal = DogBreed.valueOf(breed);
 
+		System.out.print("Enter gender: ");
 		gender = in.nextLine();
-		Gender gender = Gender.valueOf(gender);
+		Gender genderOfAnimal = Gender.valueOf(gender);
 
+		System.out.print("Enter age of dog: ");
 		age = in.nextInt(); in.nextLine();
 
-		Dog aDog = new Dog(breed, name, gender, age);
+		Dog aDog = new Dog(breedOfAnimal, name, genderOfAnimal, age);
 		aDog.toString();
+		System.out.println(aDog);
+
+		System.out.print("Enter name of cat: ");
+		name = in.nextLine();
+
+		System.out.print("Enter a breed of cat from one of the following: Persian, Bengal, Siamese, Sphynx, Ragamuffin, Toyger, LaPerm, Peterbald, Savannah, Ragdoll: ");
+		breed = in.nextLine();
+		CatBreed breedOfAnimal = CatBreed.valueOf(breed);
+
+		System.out.print("Enter gender: ");
+		gender = in.nextLine();
+		Gender genderOfAnimal = Gender.valueOf(gender);
+
+		System.out.print("Enter age of cat: ");
+		age = in.nextInt(); in.nextLine();
+
+		Cat aCat = new Cat(breedOfAnimal, name, genderOfAnimal, age);
+		aCat.toString();
+		System.out.println(aCat);
+
 
 	}
 }
