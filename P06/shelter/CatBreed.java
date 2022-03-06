49d07256f9ba7a027b0c9aeaae09ebaa99345869
Kolade Favour <fok0786@mavs.uta.edu>
2022-03-06 16:02:31 -0600

    Scrum project 02

--- P06/shelter/CatBreed.java
+++ P06/shelter/CatBreed.java
@@ -5,7 +5,6 @@ public enum CatBreed {
     Birman, British_Shorthair, Burmese, Devon_Rex, 
     Himalayan, Maine_Coon, Norwegian_Forest, Ocicat, 
     Oriental_Shorthair, Persian, Ragdoll, Russian_Blue, 
-    Scottish_Fold, Siamese, Sphynx, Tonkinese, 
-    Persian, Bengal, Siamese, Sphynx, Ragamuffin, 
-    Toyger, LaPerm, Peterbald, Savannah, Ragdoll
+    Scottish_Fold, Siamese, Sphynx, Tonkinese, Ragamuffin, 
+    Toyger, LaPerm, Peterbald, Savannah
 }
--- P06/shelter/DogBreed.java
+++ P06/shelter/DogBreed.java
@@ -4,5 +4,5 @@ public enum DogBreed {
     Mix, Labrador, Shepherd, Retriever, Bulldog, 
     Beagle, Poodle, Rottweiler, Pointer, Terrier, 
     Boxer, Dachshund, Dalmatian, Greyhound, 
-    Chihuahua, Boxer, Husky, Samoyed, Dobermann, Rottweiler, Corgi, Schnauzer
+    Chihuahua, Husky, Samoyed, Dobermann, Corgi, Schnauzer
 }
