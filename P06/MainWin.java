c9449a30f8b6bae0d15621583f543b962792183d
Kolade Favour <fok0786@mavs.uta.edu>
2022-03-06 18:02:54 -0600

    Scrum project 02

--- P06/MainWin.java
+++ P06/MainWin.java
@@ -6,11 +6,21 @@ import java.io.IOException;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 
+import shelter.Dog;
+import shelter.Cat;
+import shelter.Shelter;
+import shelter.DogBreed;
+import shelter.CatBreed;
+import shelter.Gender;
+
 public class MainWin extends JFrame {// implements ActionListener {
+	private Shelter shelter;
+    private JLabel data;
+
 	public MainWin(String title){
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(900, 900);
+		setSize(1600, 900);
 
 		JMenuBar menuBar = new JMenuBar();
 
@@ -27,6 +37,8 @@ public class MainWin extends JFrame {// implements ActionListener {
 
 		quit.addActionListener(event -> onQuitClick());
 		about.addActionListener(event -> onAboutClick());
+		dog.addActionListener(event -> onNewDogClick());
+		cat.addActionListener(event -> onNewCatClick());
 		
 		file.add(quit);
 
@@ -47,12 +59,20 @@ public class MainWin extends JFrame {// implements ActionListener {
     	dogButton.setToolTipText("Add a new dog, include name, age, breed and gender");
     	dogButton.setBorder(null);
     	toolbar.add(dogButton);
+    	dogButton.addActionListener(event -> onNewDogClick());
 
 		JButton catButton = new JButton(new ImageIcon("cat.png"));
     	catButton.setActionCommand("Add a new cat");
     	catButton.setToolTipText("Add a new cat, include name, age, breed and gender");
     	catButton.setBorder(null);
     	toolbar.add(catButton);
+    	catButton.addActionListener(event -> onNewCatClick());
+
+    	JButton shelterButton = new JButton(new ImageIcon("Animal_Shelter.png"));
+    	shelterButton.setActionCommand("List all animals");
+    	shelterButton.setToolTipText("List all animals in the shleter entered by the user");
+    	shelterButton.setBorder(null);
+    	toolbar.add(shelterButton);
 
     	getContentPane().add(toolbar, BorderLayout.PAGE_START);
 
@@ -60,11 +80,87 @@ public class MainWin extends JFrame {// implements ActionListener {
 		setVisible(true);
 	}
 
-	protected void onQuitClick() {
+	public void onQuitClick() {
 		System.exit(0);
 	}
 
-	protected void onAboutClick() {                 // Display About dialog
+	public void onNewDogClick() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(900, 900);
+
+		JComboBox breeds;
+		JTextField names;
+		JComboBox genders;
+		JSpinner ages;
+
+		JLabel breed = new JLabel("Breed");
+		String[] dogBreeds = {"Mix", "Labrador", "Shepherd", "Retriever", "Bulldog", "Beagle", "Poodle", "Rottweiler", "Pointer", "Terrier", 
+    						"Boxer", "Dachshund", "Dalmatian", "Greyhound", "Chihuahua", "Husky", "Samoyed", "Dobermann", "Corgi", "Schnauzer"
+    						};
+    	breeds = new JComboBox<String>(dogBreeds);
+
+    	JLabel name = new JLabel("<HTML><br/>Name</HTML>");
+    	names = new JTextField(50);
+
+    	JLabel gender = new JLabel("<HTML><br/>Gender</HTML>");
+    	String[] genderOfDog = {"Male", "Female"};
+    	genders = new JComboBox<String>(genderOfDog);
+
+    	JLabel age = new JLabel("<HTML><br/>Age</HTML>");
+    	SpinnerModel ageRange = new SpinnerNumberModel(0, 0, 50, 1);
+    	ages = new JSpinner(ageRange);
+
+    	Object[] objects = {breed, breeds, name, names, gender, genders, age, ages};
+
+    	int button = JOptionPane.showConfirmDialog(this, objects, "New Dog", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+    	if(button == JOptionPane.OK_OPTION){
+    		JOptionPane.showMessageDialog(this, names.getText() + " is a " + genders.getSelectedItem() + " "
+    									+ breeds.getSelectedItem() + " age " + ages.getValue()
+    									);
+    	}
+
+	}
+
+	public void onNewCatClick() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(900, 900);
+
+		JComboBox breeds;
+		JTextField names;
+		JComboBox genders;
+		JSpinner ages;
+
+		JLabel breed = new JLabel("Breed");
+		String[] catBreeds = {"Mix", "Abyssinian", "American_Shorthair", "Bengal", "Birman", "British_Shorthair", "Burmese", "Devon_Rex", 
+							"Himalayan", "Maine_Coon", "Norwegian_Forest", "Ocicat", "Oriental_Shorthair", "Persian", "Ragdoll", 
+							"Russian_Blue", "Scottish_Fold", "Siamese", "Sphynx", "Tonkinese", "Ragamuffin", "Toyger", "LaPerm", 
+							"Peterbald", "Savannah"
+    						};
+    	breeds = new JComboBox<String>(catBreeds);
+
+    	JLabel name = new JLabel("<HTML><br/>Name</HTML>");
+    	names = new JTextField(50);
+
+    	JLabel gender = new JLabel("<HTML><br/>Gender</HTML>");
+    	String[] genderOfCat = {"Male", "Female"};
+    	genders = new JComboBox<String>(genderOfCat);
+
+    	JLabel age = new JLabel("<HTML><br/>Age</HTML>");
+    	SpinnerModel ageRange = new SpinnerNumberModel(0, 0, 50, 1);
+    	ages = new JSpinner(ageRange);
+
+    	Object[] objects = {breed, breeds, name, names, gender, genders, age, ages};
+
+    	int button = JOptionPane.showConfirmDialog(this, objects, "New Dog", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+    	if(button == JOptionPane.OK_OPTION){
+    		JOptionPane.showMessageDialog(this, names.getText() + " is a " + genders.getSelectedItem() + " "
+    									+ breeds.getSelectedItem() + " age " + ages.getValue()
+    									);
+    	}
+
+	}
+
+	public void onAboutClick() {                 // Display About dialog
 		JDialog about = new JDialog();
     	about.setLayout(new FlowLayout());
     	about.setTitle("Mavs Animal Shelter Software");
@@ -82,6 +178,7 @@ public class MainWin extends JFrame {// implements ActionListener {
     	about.add(title);
 
     	JLabel artists = new JLabel("<html>"
+    	+ "<p><font size=+2>Mavs Animal Shelter Software</font></p>"
     	+ "<p>Version 1.0</p>"
     	+ "<p>Copyright 2022 by Kolade Favour</p>"
     	+ "<p>Licensed under Gnu GPL 3.0</p>");
