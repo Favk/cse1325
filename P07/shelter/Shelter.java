26874696cc317010f88f4aa05831d3a11e617594
Kolade Favour <fok0786@mavs.uta.edu>
2022-03-15 19:34:59 -0500

    Scrum project 03

---
+++ P07/shelter/Shelter.java
@@ -0,0 +1,39 @@
+package shelter;
+
+import java.util.ArrayList;
+
+public class Shelter{
+    private String name;
+    ArrayList<Animal> animals = new ArrayList<Animal>();
+
+    public Shelter(String name){
+        this.name = name;
+    }
+
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+    }
+
+    public int numAnimals(){
+        return animals.size();
+    }
+
+    public Animal getAnimal(int index){
+        if(index < 0){
+            throw new IllegalArgumentException("Index cannot be negative " + index);
+        }
+
+        return animals.get(index);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+
+        for(Animal a : animals){
+            str.append("\n");
+            str.append(a.toString());
+        }
+        return str.toString();
+    }
+}
