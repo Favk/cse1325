a8efcff572652214c65beba485a98f0274894b65
Kolade Favour <fok0786@mavs.uta.edu>
2022-04-10 23:21:07 -0500

    P09 color.cpp final draft

--- P09/full_credit/color.cpp
+++ P09/full_credit/color.cpp
@@ -5,12 +5,12 @@ Color::Color(int red, int green, int blue)
 			: _red{red}, _green{green}, _blue{blue} {}
 
 std::string Color::to_string(){
-	std::ostringstream words;
-	words << "(" << _red << "," << _green << "," << _blue << ")";
-	return words.str();
+	std::ostringstream colors;
+	colors << "(" << _red << "," << _green << "," << _blue << ")";
+	return colors.str();
 }
 
 std::string Color::colorize(std::string text){
-	return "\x1b[38;2;" + std::to_string(_red) + ";" + std::to_string(_green) + ";" + std::to_string(_blue) + "m"
-			+ text + "\x1b[0m";
+	return "\033[38;2;" + std::to_string(_red) + ";" + std::to_string(_green) + ";" + std::to_string(_blue) + "m"
+			+ text + "\033[0m";
 }
